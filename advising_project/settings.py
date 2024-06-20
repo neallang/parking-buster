47f12bf0c8822b1b1bb4839a9db877138c9a5153
Neal Langhorne <neal_langhorne@yahoo.com>
2024-06-20 14:33:15 -0400

    added host site

--- advising_project/settings.py
+++ advising_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','advising-app-0d36361fa73a.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','advising-app-0d36361fa73a.herokuapp.com', 'neallang.pythonanywhere.com']
 
 
 # Application definition
